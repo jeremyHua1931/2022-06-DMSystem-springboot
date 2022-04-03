636f8f27ec0f66aaf56eb75167c50be0a930459a
jeremyhua <jeremyhua@foxmail.com>
2022-04-03 17:40:55 +0800

    init  springboot

---
+++ src/main/java/com/documentmanagementsystem/DocumentManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.documentmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/documentmanagementsystem/DocumentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.documentmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
